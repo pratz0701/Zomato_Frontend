60860dd7101f2428b3971b16721de8c32fa8f7f7
Anjali <anjalisah282001@gmail.com>
2022-11-02 17:20:08 +0530

    added menu api

--- src/components/common/menu/index.js
+++ src/components/common/menu/index.js
@@ -1,106 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 
 const Menu = () => {
-  return (
-    <div class="main max-width">
-      <div class="section-title">
-        <h2>our menu</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, eum!
-        </p>
-      </div>
-      <div class="menus">
-        <div class="menu-column">
-          <div class="single-menu">
-            <img src="https://via.placeholder.com/150C" alt="" />
-            <div class="menu-content">
-              <h5>
-                food title <span>$50</span>
-              </h5>
-              <p>Lorem ipsum dolor sit amet.</p>
+    const [menu, setMenu] = useState([]);
+    useEffect(() => {
+
+        const fetchMenu = async () => {
+            const res = await fetch(`http://3.111.196.246/main/all_res_dishes/?res_id=${window.location.href.split('/').pop()}`)
+            const data = await res.json();
+            setMenu(data.response);
+            
+        }
+        fetchMenu();
+    }, [])
+
+    return (
+        <div class="main max-width">
+            <div class="section-title">
+                <h2>our menu</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, eum!
+                </p>
             </div>
-          </div>
-          <div class="single-menu">
-            <img src="https://via.placeholder.com/150C" alt="" />
-            <div class="menu-content">
-              <h5>
-                food title <span>$50</span>
-              </h5>
-              <p>Lorem ipsum dolor sit amet.</p>
+            <div class="menus">
+                {
+                    menu.map(e => {
+                        return (
+                            <>
+                                <div className="bada_bada max-width"></div>
+                                <div class="single-menu" key={e.food_id}>
+                                    <img src="https://via.placeholder.com/150C" alt="" />
+                                    <div class="menu-content">
+                                        <h5>
+                                            {e.food_name} <span>Rs {e.price}</span>
+                                        </h5>
+                                        <p>Lorem ipsum dolor sit amet.</p>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    })
+                }
             </div>
-          </div>
-          <div class="single-menu">
-            <img src="https://via.placeholder.com/150C" alt="" />
-            <div class="menu-content">
-              <h5>
-                food title <span>$50</span>
-              </h5>
-              <p>Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
         </div>
-        <div class="menu-column">
-          <div class="single-menu">
-            <img src="https://via.placeholder.com/150C" alt />
-            <div class="menu-content">
-              <h5>
-                food title <span>$50</span>
-              </h5>
-              <p>Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
-          <div class="single-menu">
-            <img src="https://via.placeholder.com/150C" alt="" />
-            <div class="menu-content">
-              <h5>
-                food title <span>$50</span>
-              </h5>
-              <p>Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
-          <div class="single-menu">
-            <img src="https://via.placeholder.com/150C" alt="" />
-            <div class="menu-content">
-              <h5>
-                food title <span>$50</span>
-              </h5>
-              <p>Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
-        </div>
-        <div class="menu-column">
-          <div class="single-menu">
-            <img src="https://via.placeholder.com/150C" alt="" />
-            <div class="menu-content">
-              <h5>
-                food title <span>$50</span>
-              </h5>
-              <p>Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
-          <div class="single-menu">
-            <img src="https://via.placeholder.com/150C" alt="" />
-            <div class="menu-content">
-              <h5>
-                food title <span>$50</span>
-              </h5>
-              <p>Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
-          <div class="single-menu">
-            <img src="https://via.placeholder.com/150C" alt="" />
-            <div class="menu-content">
-              <h5>
-                food title <span>$50</span>
-              </h5>
-              <p>Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Menu;
